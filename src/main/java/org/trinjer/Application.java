71b2928a46bc9df6e027e02d945c43f4747c6112
arturjoshi <arturjoshi.mailbox@gmail.com>
2017-07-06 21:37:18 +0300

    added profiles

--- src/main/java/org/trinjer/Application.java
+++ src/main/java/org/trinjer/Application.java
@@ -2,11 +2,13 @@ package org.trinjer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource({"classpath:${spring.profiles.active}/db.properties"})
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
